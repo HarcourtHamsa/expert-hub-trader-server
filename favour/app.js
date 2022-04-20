e9ec392d23cdd20fab81f794958b95936d4cf491
Harcourt Hamsa <hamsaharcourt@gmail.com>
2022-04-20 18:56:53 +0100

    initial commit

--- favour/app.js
+++ favour/app.js
@@ -1,8 +1,11 @@
 const menu = document.querySelector("#menu");
 const navLinks = document.querySelector("#nav_links");
+const customCursor = document.querySelector("#cursor");
 
 menu.addEventListener("click", (e) => {
-    navLinks.toggleAttribute("active")
+  navLinks.toggleAttribute("active");
 });
 
+const whatSection = document.querySelector("#what");
+console.log(whatSection.clientTop);
 
